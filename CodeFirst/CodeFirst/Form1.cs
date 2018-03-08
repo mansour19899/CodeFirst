770aec028ba690c5f29118e4a299ef28e5c59c2c
mansour <smm.2020@gmail.com>
2018-03-08 13:41:13 +0330

    ایجاد کلاس برای ایجاد فیلدهای جدول

--- CodeFirst/CodeFirst/Form1.cs
+++ CodeFirst/CodeFirst/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,43 @@ namespace CodeFirst
 {
     public partial class Form1 : Form
     {
+        public class Course
+        {
+            public int Id { get; set; }
+            public string Tittle { get; set; }
+            public string Description { get; set; }
+            public CourseLevel Level { get; set; }
+            public float FullPrice { get; set; }
+            public  Author Author { get; set; }
+            public IList<Tagg> Taggs { get; set; }
+            }
+        public class Author
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public IList<Course> Courses { get; set; }
+        }
+        public class Tagg
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public IList<Course>  Courses { get; set; }
+        }
+
+        public enum CourseLevel
+        {
+            Beginner=1,
+            Intermediate=2,
+            Advanced=3
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
